6b6f1b869a9c6c7f9dd230d42c2804a4607dc170
wcaughey1984 <william.g.caughey.1984@gmail.com>
2020-08-14 15:43:51 -0600

    update

--- MakeoverMonday2020w31/Makeover Monday 2020w31.R
+++ MakeoverMonday2020w31/Makeover Monday 2020w31.R
@@ -9,10 +9,20 @@
 library(tidyverse)
 library(httr)
 library(readxl)
+library(lubridate)
 
 ##### Bring in data #####
 
 url <- "https://query.data.world/s/igxj3pmxgtrtfum2lorsoedczkkcao"
-invisible(capture.output(GET(url, write_disk(tf <- tempfile(fileext = ".xls"))))
-df <- read_excel(tf)
-
+invisible(capture.output(GET(url, write_disk(tf <- tempfile(fileext = ".xls")))))
+df <- read_excel(tf) %>%
+    filter(grepl(pattern = "[0-9][0-9][0-9][0-9]\\s[A-Z][A-Z][A-Z]", 
+                 x = Title)) %>%
+    rowwise() %>%
+    mutate(year = str_split(string = Title, pattern = " ")[[1]][1],
+           month = str_split(string = Title, pattern = " ")[[1]][2],
+           qtr = ifelse(month %in% c("JAN", "FEB", "MAR"), "Qtr1",
+                 ifelse(month %in% c("APR", "MAY", "JUN"), "Qtr2",
+                 ifelse(month %in% c("JUL", "AUG", "SEP"), "Qtr3", "Qtr4"))))
+          
+          
